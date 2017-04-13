e296abddef5df27bdbdf8e4479ca6e6f41f19fe3
Dan Sun <dan.sun@canterbury.ac.nz>
2017-04-13 13:37:38 +1200

    Using more generic file names.

--- generate-security-certs.bash
+++ generate-security-certs.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 echo "Generating an SSL private key to sign your certificate..."
-openssl genrsa -des3 -out jupyterhub-econ-rcc.key 1024
+openssl genrsa -des3 -out myssl.key 1024
 
 echo "Generating a Certificate Signing Request..."
-openssl req -new -key jupyterhub-econ-rcc.key -out jupyterhub-econ-rcc.csr
+openssl req -new -key myssl.key -out myssl.csr
 
 echo "Removing passphrase from key (for nginx)..."
-cp jupyterhub-econ-rcc.key jupyterhub-econ-rcc.key.org
-openssl rsa -in jupyterhub-econ-rcc.key.org -out jupyterhub-econ-rcc.key
-rm jupyterhub-econ-rcc.key.org
+cp myssl.key myssl.key.org
+openssl rsa -in myssl.key.org -out myssl.key
+rm myssl.key.org
 
 echo "Generating certificate..."
-openssl x509 -req -days 365 -in jupyterhub-econ-rcc.csr -signkey jupyterhub-econ-rcc.key -out jupyterhub-econ-rcc.crt
+openssl x509 -req -days 365 -in myssl.csr -signkey myssl.key -out myssl.crt
 
-echo "Copying certificate (jupyterhub-econ-rcc.crt) to ./security/"
-cp jupyterhub-econ-rcc.crt ./security/ssl.crt
+echo "Copying certificate (myssl.crt) to ./security/"
+cp myssl.crt ./security/ssl.crt
 
 echo "Copying key (myssl.key) to ./security/"
-cp jupyterhub-econ-rcc.key ./security/ssl.key
+cp myssl.key ./security/ssl.key
 
 echo "Generating Cokkie secret"
 openssl rand -hex 1024 > ./security/cookie_secret
